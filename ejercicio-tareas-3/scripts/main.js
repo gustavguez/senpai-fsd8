97597bfc0e088f812ff27963da9c0c1b7310da13
Gustavo Rodriguez <gustavguez@gmail.com>
2021-12-07 21:46:34 -0300

    feat(tareas3): borrar básico y listado inicial

---
+++ ejercicio-tareas-3/scripts/main.js
@@ -0,0 +1,105 @@
+//Mis tareas
+const misTareas = [
+    {
+        description: 'Estudiar CSS'
+    },
+    {
+        description: 'Estudiar JS'
+    },
+    {
+        description: 'Estudiar HTML'
+    }
+];
+
+//Selecciono el formulario
+const mainForm = document.querySelector('.js-task-form');
+
+//Selecciono el input del formulario
+const inputForm = document.querySelector('.js-task-form input');
+
+//Seleccionar mi contenedor de tareas
+const taskList = document.querySelector('.js-task-list');
+
+//Seleccionar el botón que borra todas las tareas
+const clearAllTasks = document.querySelector('.js-clear-all');
+
+//Escuchar el evento submit
+mainForm.addEventListener(
+    'submit', 
+    function(eventoSubmit){
+        //Guardamos el valor del input
+        const inputValue = inputForm.value;
+
+        //Crear el elemento de la tarea
+        crearTareaElemento(inputValue);
+
+        //Limpio el value de la caja de texto
+        inputForm.value = '';
+
+        //Detenemos el comportamiento por defecto de los forms
+        eventoSubmit.preventDefault();
+    }
+);
+
+//Escuchar el click en el botón clearAll
+clearAllTasks.addEventListener(
+    'click',
+    function(miInfoDelEvento){
+        //Borro el innerHTML
+        taskList.innerHTML = '';
+    }
+);
+
+//Creamos una funcion para encapsular la logíca de crear el li
+function crearTareaElemento(tarea){
+    //Crear un li para luego agregarle el texto del usuario
+    const tareaLi = document.createElement('li');
+
+    //Agregamos la clase task al li
+    tareaLi.classList.add('task');
+
+    //Crear un span para indicar el texto
+    const tareaSpan = document.createElement('span');
+    tareaSpan.textContent = tarea;
+    
+    //Creamos un a pra poder borrarla individualmente
+    const tareaA = document.createElement('a');
+    tareaA.classList.add('delete-task');
+
+    //Crear un span para el icono del a
+    const tareaAIcon = document.createElement('span');
+    tareaAIcon.classList.add('icon');
+
+    //Agregamos el span.icon al a
+    tareaA.appendChild(tareaAIcon);
+
+    //Agregar el span y el a como hijo del li
+    tareaLi.append(
+        tareaSpan, 
+        tareaA
+    );
+
+    //Espero el click en el botón de borrar
+    tareaA.addEventListener(
+        'click',
+        function(event){
+            //A que fue clickeado
+            const target = event.target;
+
+            //Guardo mi padre li
+            const tareaLiPadre = target.parentNode;
+            
+            //Remover del DOM
+            tareaLiPadre.remove();
+        }
+    );
+    
+    //Agrego mi li al DOM
+    taskList.appendChild(tareaLi);
+}
+
+//Creamos las tareas iniciales
+for (const tareaObj of misTareas) {
+    //Creo un elemento por cada tarea llamando la función
+    crearTareaElemento(tareaObj.description);
+};

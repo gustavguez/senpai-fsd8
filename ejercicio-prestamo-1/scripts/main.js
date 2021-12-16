094b117d1679f648b5d71bd549335af517380656
Gustavo Rodriguez <gustavguez@gmail.com>
2021-12-16 19:58:26 -0300

    feat(Clase23): add base files

---
+++ ejercicio-prestamo-1/scripts/main.js
@@ -0,0 +1,39 @@
+const montoElement = document.querySelector('.js-mont');
+montoElement.value = '100';
+
+// //Funcion para solicitar un número
+// function solicitarMonto(){
+//     let monto = prompt('Ingrese el monto');
+
+//     if (monto < 100){
+//         //Le asignamos a prepo 100
+//         monto = 100;
+//     }
+//     return monto;
+// }
+
+// //Solicito los números
+// const misMontos = [];
+// for (let contador = 0; contador < 2; contador++) {
+//     let monto = solicitarMonto();
+
+//     //Lo agrego al array
+//     misMontos.push(monto);
+// }
+
+// //Recorro mis montos
+// let total = 0;
+// for (const miMonto of misMontos) {
+//     total = total + parseInt(miMonto);
+// }
+// console.log(total);
+
+// // const cuotas = 6;
+// // let resultado = null;
+
+// // console.log(monto1, monto2, monto3)
+
+// //Resultado
+// // resultado = monto1 / cuotas;
+
+
